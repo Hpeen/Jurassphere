547feeac45fe6c15cca8796526d7938d5da14470
Hpeen <creepertheripper@gmail.com>
2026-07-01 23:12:54 +0300

    feat: add intro animation and toggleable ambient audio

--- js/main.js
+++ js/main.js
@@ -2,10 +2,13 @@ import { loadSites } from "./data.js";
 import { createGlobe, setPoints, flyTo } from "./globe.js";
 import { initPanel, openPanel } from "./panel.js";
 import { filterByEra, searchSites } from "./filters.js";
+import { playIntro, initAudioToggle } from "./audio.js";
 
 const el = document.getElementById("globe");
 const statusEl = document.getElementById("status");
 initPanel();
+playIntro();
+initAudioToggle();
 const globe = createGlobe(el, (site) => openPanel(site));
 
 let allSites = [];
