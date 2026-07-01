983a6a4a3c77325d78fda1a227ab21f388797637
Hpeen <creepertheripper@gmail.com>
2026-07-01 23:03:18 +0300

    feat: render fossil sites on an auto-spinning 3D globe with fly-to

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log("Jurassphere booting…");
+import { loadSites } from "./data.js";
+import { createGlobe, setPoints, flyTo } from "./globe.js";
+
+const el = document.getElementById("globe");
+const globe = createGlobe(el, (site) => console.log("clicked", site.title));
+
+loadSites().then(({ sites, source }) => {
+  console.log(`Loaded ${sites.length} sites from ${source}`);
+  setPoints(globe, sites);
+});
+
+window._flyTo = (lat, lng) => flyTo(globe, lat, lng);
